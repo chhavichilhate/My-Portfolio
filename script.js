7432a4dbc199e86fb5946cad812dd9832183271c
chhavichilhate <chhavi.chilhate12@gmail.com>
2025-10-30 20:31:42 +0530

    Add new files

---
+++ script.js
@@ -0,0 +1,12 @@
+
+  // const viewer = document.getElementById('resumeViewer');
+  // if (viewer.style.display === 'none' || viewer.style.display === '') {
+  //   viewer.style.display = 'block';
+  // } else {
+  //   viewer.style.display = 'none';
+  // }
+
+  document.getElementById('contactForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Thank you for your message !');
+});
